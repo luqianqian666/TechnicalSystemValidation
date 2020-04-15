879e3d260fabbd39931c0a8d8175949c77433b11
luqianqian <541971057@qq.com>
2020-04-15 17:42:13 +0800

    little

--- TeSystemProject/static/config.js
+++ TeSystemProject/static/config.js
@@ -1,8 +1,14 @@
 const config = {
-  global_url: 'http://127.0.0.1:8080/',  //孟小峰服务器地址
+  global_url: 'http://127.0.0.1:8069/',  //孟小峰服务器地址
   
   getTaskUrl:function(){
     return this.global_url+"api/v1.0/task"
+  },
+
+  getTasks:function(){
+    var getTimestamp=new Date().getTime();  
+    global_url=global_url+"?timestamp="+getTimestamp ;
+    return global_url; 
   }
  
 };
